fa64c09b3de6fcccbc303ff94ec369893e84cc43
YanoRyo <yanoryo7032@gmail.com>
2023-10-14 16:11:18 +0900

    Rendiring the Item list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Jackets", quantity: 2, packed: true },
+  { id: 4, description: "BaseBall", quantity: 12, packed: false },
+  { id: 5, description: "iPhone", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -20,7 +28,25 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
